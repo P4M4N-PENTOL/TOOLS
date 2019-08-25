ed8baad32b3d4abc37ccd767dc36efd616986390
p4m4npentol <54494543+p4m4npentol@users.noreply.github.com>
2019-08-25 15:23:17 +0800

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,553 @@
+clear
+merah='\e[38;5;1m'
+hijau='\e[38;5;2m'
+kuning='\e[38;5;226m'
+pink='\e[38;5;201m'
+oren='\e[38;5;202m'
+biru='\e[38;5;4m'
+cyan='\e[38;5;1m'
+ungu='\e[38;5;93m'
+grey='\e[38;5;243m'
+putih='\e[38;5;255m'
+echo -e $kuning'           ╔══╗────────────────╔══╗─────────╔╗─────╔╗─'
+sleep 0.2
+echo -e $kuning'           ║╔╗║────────────────║╔╗║────────╔╝╚╗────║║─'
+sleep 0.2
+echo -e $kuning'           ║╚╝║╔══╗╔╗╔╗╔══╗╔═╗─║╚╝║╔══╗╔═╗─╚╗╔╝╔══╗║║─'
+sleep 0.2
+echo -e $kuning'           ║╔═╝║╔╗║║╚╝║║╔╗║║╔╗╗║╔═╝║║═╣║╔╗╗─║║─║╔╗║║║─'
+sleep 0.2
+echo -e $kuning'           ║║──║╔╗║║║║║║╔╗║║║║║║║──║║═╣║║║║─║╚╗║╚╝║║╚╗'
+sleep 0.2
+echo -e $kuning'           ╚╝──╚╝╚╝╚╩╩╝╚╝╚╝╚╝╚╝╚╝──╚══╝╚╝╚╝─╚═╝╚══╝╚═╝'
+sleep 0.2
+echo ⃟ ⃟ ⃟ ⃟ ════════════━━━━━━━━━─────⊰𖣔⊱─────━━━━━━━━━━════════════ ⃟ ⃟ ⃟ | lolcat
+echo
+echo -e $cyan'  Coded By : P4M4NPENTOL'
+sleep 0.3
+echo -e $ungu'  Author   : P4M4NPENTOL'
+sleep 0.3
+echo -e $pink'  Group    : Anonymous Cyber Army'
+sleep 0.3
+echo -e $kuning'  Forum    : Anonymous Indonesia'
+sleep 0.3
+echo -e $biru'  Github   : http://github.com/P4M4NPENTOL'
+sleep 0.3
+echo
+echo ⃟ ⃟ ⃟ ⃟ ════════════━━━━━━━━━─────⊰𖣔⊱─────━━━━━━━━━━════════════ ⃟ ⃟ ⃟ | lolcat
+echo
+echo
+echo
+echo -e $oren'            ──────▄▀▄─────▄▀▄──────'
+echo -e $oren'            ─────▄█░░▀▀▀▀▀░░█▄─────'
+echo -e $oren'            ─▄▄──█░░░░░░░░░░░█──▄▄─'
+echo -e $oren'            █▄▄█─█░░▀░░┬░░▀░░█─█▄▄█'
+echo -e $hijau' ╔════╦═════════════════════════════════════╗'
+echo -e $hijau' ║'$kuning' 01'$hijau' ║'$putih' UPDATE PACKAGES                     '$hijau'║'
+echo -e $hijau' ╠════╬═════════════════════════════════════╝'
+sleep 0.2
+echo -e $hijau' ╠════╬═════════════════════════════════════╗'
+echo -e $hijau' ║'$kuning' 02'$hijau' ║'$putih' INSTALL PACKAGES                    '$hijau'║'
+echo -e $hijau' ╠════╬═════════════════════════════════════╝'
+sleep 0.2
+echo -e $hijau' ╠════╬═════════════════════════════════════╗'
+echo -e $hijau' ║'$kuning' 03'$hijau' ║'$putih' NUYUL YODO                          '$hijau'║'
+echo -e $hijau' ╠════╬═════════════════════════════════════╝'
+sleep 0.2
+echo -e $hijau' ╠════╬═════════════════════════════════════╗'
+echo -e $hijau' ║'$kuning' 04'$hijau' ║'$putih' NUYUL VEEU 1                        '$hijau'║'
+echo -e $hijau' ╠════╬═════════════════════════════════════╝'
+sleep 0.2
+echo -e $hijau' ╠════╬═════════════════════════════════════╗'
+echo -e $hijau' ║'$kuning' 05'$hijau' ║'$putih' NUYUL VEEU 2                        '$hijau'║'
+echo -e $hijau' ╠════╬═════════════════════════════════════╝'
+sleep 0.2
+echo -e $hijau' ╠════╬═════════════════════════════════════╗'
+echo -e $hijau' ║'$kuning' 06'$hijau' ║'$putih' TOOLS TUANBADUT                     '$hijau'║'
+echo -e $hijau' ╠════╬═════════════════════════════════════╝'
+sleep 0.2
+echo -e $hijau' ╠════╬═════════════════════════════════════╗'
+echo -e $hijau' ║'$kuning' 07'$hijau' ║'$putih' TOOLS BLACK DRAGON                  '$hijau'║'
+echo -e $hijau' ╠════╬═════════════════════════════════════╝'
+sleep 0.2
+echo -e $hijau' ╠════╬═════════════════════════════════════╗'
+echo -e $hijau' ║'$kuning' 08'$hijau' ║'$putih' TOOLS BAJINGAN                      '$hijau'║'
+echo -e $hijau' ╠════╬═════════════════════════════════════╝'
+sleep 0.2
+echo -e $hijau' ╠════╬═════════════════════════════════════╗'
+echo -e $hijau' ║'$kuning' 09'$hijau' ║'$putih' TOOLS RUSMANA                       '$hijau'║'
+echo -e $hijau' ╠════╬═════════════════════════════════════╝'
+sleep 0.2
+echo -e $hijau' ╠════╬═════════════════════════════════════╗'
+echo -e $hijau' ║'$kuning' 10'$hijau' ║'$putih' DDOS                                '$hijau'║'
+echo -e $hijau' ╠════╬═════════════════════════════════════╝'
+sleep 0.2
+echo -e $hijau' ╠════╬═════════════════════════════════════╗'
+echo -e $hijau' ║'$kuning' 11'$hijau' ║'$putih' INSTALL METASPLOIT                  '$hijau'║'
+echo -e $hijau' ╠════╬═════════════════════════════════════╝'
+sleep 0.2
+echo -e $hijau' ╠════╬═════════════════════════════════════╗'
+echo -e $hijau' ║'$kuning' 12'$hijau' ║'$putih' METASPLOIT                          '$hijau'║'
+echo -e $hijau' ╠════╬═════════════════════════════════════╝'
+sleep 0.2
+echo -e $hijau' ╠════╬═════════════════════════════════════╗'
+echo -e $hijau' ║'$kuning' 13'$hijau' ║'$putih' INSTALL NGROK                       '$hijau'║'
+echo -e $hijau' ╠════╬═════════════════════════════════════╝'
+sleep 0.2
+echo -e $hijau' ╠════╬═════════════════════════════════════╗'
+echo -e $hijau' ║'$kuning' 14'$hijau' ║'$putih' NGROK                               '$hijau'║'
+echo -e $hijau' ╠════╬═════════════════════════════════════╝'
+sleep 0.2
+echo -e $hijau' ╠════╬═════════════════════════════════════╗'
+echo -e $hijau' ║'$kuning' 15'$hijau' ║'$putih' TOOL-X                              '$hijau'║'
+echo -e $hijau' ╠════╬═════════════════════════════════════╝'
+sleep 0.2
+echo -e $hijau' ╠════╬═════════════════════════════════════╗'
+echo -e $hijau' ║'$kuning' 16'$hijau' ║'$putih' REDHAWK                             '$hijau'║'
+echo -e $hijau' ╠════╬═════════════════════════════════════╝'
+sleep 0.2
+echo -e $hijau' ╠════╬═════════════════════════════════════╗'
+echo -e $hijau' ║'$kuning' 17'$hijau' ║'$putih' SQLMAP                              '$hijau'║'
+echo -e $hijau' ╠════╬═════════════════════════════════════╝'
+sleep 0.2
+echo -e $hijau' ╠════╬═════════════════════════════════════╗'
+echo -e $hijau' ║'$kuning' 18'$hijau' ║'$putih' LAZYMUX                             '$hijau'║'
+echo -e $hijau' ╠════╬═════════════════════════════════════╝'
+sleep 0.2
+echo -e $hijau' ╠════╬═════════════════════════════════════╗'
+echo -e $hijau' ║'$kuning' 19'$hijau' ║'$putih' DEFACE                              '$hijau'║'
+echo -e $hijau' ╠════╬═════════════════════════════════════╝'
+sleep 0.2
+echo -e $hijau' ╠════╬═════════════════════════════════════╗'
+echo -e $hijau' ║'$kuning' 20'$hijau' ║'$putih' OPEN SOURCE FACEBOOK                '$hijau'║'
+echo -e $hijau' ╠════╬═════════════════════════════════════╝'
+sleep 0.2
+echo -e $hijau' ╠════╬═════════════════════════════════════╗'
+echo -e $hijau' ║'$kuning' 21'$hijau' ║'$putih' HACK CAMERA                         '$hijau'║'
+echo -e $hijau' ╠════╬═════════════════════════════════════╝'
+sleep 0.2
+echo -e $hijau' ╠════╬═════════════════════════════════════╗'
+echo -e $hijau' ║'$kuning' 22'$hijau' ║'$putih' SCRIPT DEFACE CREATOR               '$hijau'║'
+echo -e $hijau' ╠════╬═════════════════════════════════════╝'
+sleep 0.2
+echo -e $hijau' ╠════╬═════════════════════════════════════╗'
+echo -e $hijau' ║'$kuning' 23'$hijau' ║'$putih' SPAMMER                             '$hijau'║'
+echo -e $hijau' ╠════╬═════════════════════════════════════╝'
+sleep 0.2
+echo -e $hijau' ╠════╬═════════════════════════════════════╗'
+echo -e $hijau' ║'$kuning' 24'$hijau' ║'$putih' SPAM SMS                            '$hijau'║'
+echo -e $hijau' ╠════╬═════════════════════════════════════╝'
+sleep 0.2
+echo -e $hijau' ╠════╬═════════════════════════════════════╗'
+echo -e $hijau' ║'$kuning' 25'$hijau' ║'$putih' SPAM TELPON                         '$hijau'║'
+echo -e $hijau' ╠════╬═════════════════════════════════════╝'
+sleep 0.2
+echo -e $hijau' ╠════╬═════════════════════════════════════╗'
+echo -e $hijau' ║'$kuning' 26'$hijau' ║'$putih' BLACK FB PREMIUM                    '$hijau'║'
+echo -e $hijau' ╠════╬═════════════════════════════════════╝'
+sleep 0.2
+echo -e $hijau' ╠════╬═════════════════════════════════════╗'
+echo -e $hijau' ║'$kuning' 27'$hijau' ║'$putih' DARK FB                             '$hijau'║'
+echo -e $hijau' ╠════╬═════════════════════════════════════╝'
+sleep 0.2
+echo -e $hijau' ╠════╬═════════════════════════════════════╗'
+echo -e $hijau' ║'$kuning' 28'$hijau' ║'$putih' DARK FB 2                           '$hijau'║'
+echo -e $hijau' ╠════╬═════════════════════════════════════╝'
+sleep 0.2
+echo -e $hijau' ╠════╬═════════════════════════════════════╗'
+echo -e $hijau' ║'$kuning' 29'$hijau' ║'$putih' INSTALL OH-MY-ZSH                   '$hijau'║'
+echo -e $hijau' ╠════╬═════════════════════════════════════╝'
+sleep 0.2
+echo -e $hijau' ╠════╬═════════════════════════════════════╗'
+echo -e $hijau' ║'$kuning' 30'$hijau' ║'$putih' SANTET ONLINE                       '$hijau'║'
+echo -e $hijau' ╠════╬═════════════════════════════════════╝'
+sleep 0.2
+echo -e $hijau' ╠════╬═════════════════════════════════════╗'
+echo -e $hijau' ║'$kuning' 31'$hijau' ║'$putih' HACK CAMERA                         '$hijau'║'
+echo -e $hijau' ╠════╬═════════════════════════════════════╝'
+sleep 0.2
+echo -e $hijau' ╠════╬═════════════════════════════════════╗'
+echo -e $hijau' ║'$kuning' 32'$hijau' ║'$putih' TOOLS GABUT                         '$hijau'║'
+echo -e $hijau' ╠════╬═════════════════════════════════════╝'
+sleep 0.2
+echo -e $hijau' ╠════╬═════════════════════════════════════╗'
+echo -e $hijau' ║'$kuning' 33'$hijau' ║'$putih' MEMPERCANTIK TAMPILAN TERMUX        '$hijau'║'
+echo -e $hijau' ╠════╬═════════════════════════════════════╝'
+sleep 0.2
+echo -e $hijau' ╠════╬═════════════════════════════════════╗'
+echo -e $hijau' ║'$kuning' 34'$hijau' ║'$putih' MEMPERCANTIK TAMPILAN TERMUX 2      '$hijau'║'
+echo -e $hijau' ╠════╬═════════════════════════════════════╝'
+sleep 0.2
+echo -e $hijau' ╠════╬═════════════════════════════════════╗'
+echo -e $hijau' ║'$kuning' 35'$hijau' ║'$putih' UBAH PROMPT TEMUX                   '$hijau'║'
+echo -e $hijau' ╠════╬═════════════════════════════════════╝'
+sleep 0.2
+echo -e $hijau' ╠════╬═════════════════════════════════════╗'
+echo -e $hijau' ║'$kuning' 36'$hijau' ║'$putih' HACK NASA                           '$hijau'║'
+echo -e $hijau' ╠════╬═════════════════════════════════════╝'
+sleep 0.2
+echo -e $hijau' ╠════╬═════════════════════════════════════╗'
+echo -e $hijau' ║'$kuning' 37'$hijau' ║'$putih' HACK WHATSAPP                       '$hijau'║'
+echo -e $hijau' ╠════╬═════════════════════════════════════╝'
+sleep 0.2
+echo -e $hijau' ╠════╬═════════════════════════════════════╗'
+echo -e $hijau' ║'$kuning' 00'$hijau' ║'$putih' TELKOMSEL GRATIS                    '$hijau'║'
+echo -e $hijau' ╚════╩═════════════════════════════════════╝'
+sleep 0.2
+echo $biru
+read -p " MASUKKAN NOMER: " pilih
+
+if [ $pilih = 1 ] || [ $pilih = 1 ]
+then
+clear
+apt update
+apt upgrade
+pkg update
+pkg upgrade
+cd
+sleep 5
+sh run
+fi
+
+if [ $pilih = 2 ] || [ $pilih = 2 ]
+then
+clear
+pkg install root-repo -y unstable-repo -y x11-repo -y nmap -y git -y ruby -y python -y python2 -y curl -y wget -y toilet -y cowsay -y figlet  -y php -y nano -y mc -y neofetch -y screenfetch -y openssl -y libcurl -y proot -y tor -y
+gem install lolcat
+pip2 install urllib3 chardet certifi idna requests mechanize
+cd
+sleep 5
+fi
+
+if [ $pilih = 3 ] || [ $pilih = 3 ]
+then
+clear
+cd .cache
+cd yodo
+php bot.php
+cd
+sh run
+fi
+
+if [ $pilih = 4 ] || [ $pilih = 4 ]
+then
+clear
+cd .cache
+cd veeu
+php run.php
+sleep 2
+cd
+sh run
+fi
+
+if [ $pilih = 5 ] || [ $pilih = 5 ]
+then
+clear
+cd .cache
+cd veeu2
+php bot.php
+cd
+sh run
+fi
+
+if [ $pilih = 6 ] || [ $pilih = 6 ]
+then
+clear
+cd .cache
+cd tuanbadut
+sh run.sh
+sleep 5
+cd
+sh run
+fi
+
+if [ $pilih = 7 ] || [ $pilih = 7 ]
+then
+clear
+cd .cache
+cd dragon
+sh run.sh
+cd
+sh run
+fi
+
+if [ $pilih = 8 ] || [ $pilih = 8 ]
+then
+clear
+cd .cache
+cd bajingan
+sh run.sh
+cd
+sh run
+fi
+
+if [ $pilih = 9 ] || [ $pilih = 9 ]
+then
+clear
+cd .cache
+cd rus
+sh run.sh
+cd
+sh run
+fi
+
+if [ $pilih = 10 ] || [ $pilih = 10 ]
+then
+clear
+cd .cache
+cd DDOS
+sh DDOS.sh
+cd
+sh run
+fi
+
+if [ $pilih = 11 ] || [ $pilih = 11 ]
+then
+clear
+cd $HOME
+pkg install unstable-repo
+pkg install metasploit
+sleep 5
+cd
+sh run
+fi
+
+if [ $pilih = 12 ] || [ $pilih = 12 ]
+then
+clear
+cd $HOME
+msfconsole
+sleep 5
+cd
+sh run
+fi
+
+if [ $pilih = 13 ] || [ $pilih = 13 ]
+then
+clear
+cd $HOME
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
+unzip ngrok-stable-linux-arm.zip
+./ngrok authtoken 1OyJ0EYDZKnwrfc175vIlRptgMg_5DksTE1m3t4HUMb3U6Sx1
+cd
+sh run
+fi
+
+if [ $pilih = 14 ] || [ $pilih = 14 ]
+then
+clear
+cd $HOME
+./ngrok tcp 4444
+cd
+sh run
+
+fi
+
+if [ $pilih = 15 ] || [ $pilih = 15 ]
+then
+clear
+cd .cache
+Tool-X
+cd
+sh run
+fi
+
+if [ $pilih = 16 ] || [ $pilih = 16 ]
+then
+clear
+cd .cache
+cd redhawk
+php rhawk.php
+cd
+sh run
+fi
+
+if [ $pilih = 17 ] || [ $pilih = 17 ]
+then
+clear
+cd .cache
+cd sqlmap
+python2 sqlmap.py
+cd
+sh run
+fi
+
+if [ $pilih = 18 ] || [ $pilih = 18 ]
+then
+clear
+cd .cache
+cd lazymux
+python2 lazymux.py
+cd
+sh run
+fi
+
+if [ $pilih = 19 ] || [ $pilih = 19 ]
+then
+clear
+
+fi
+
+if [ $pilih = 20 ] || [ $pilih = 20 ]
+then
+clear
+cd .cache
+cd OSIF
+python2 osif.py
+cd
+sh run
+fi
+
+
+if [ $pilih = 21 ] || [ $pilih = 21 ]
+then
+clear
+cd .cache
+cd camera
+sh run saycheese.sh
+cd
+sh run
+fi
+
+
+if [ $pilih = 22 ] || [ $pilih = 22 ]
+then
+clear
+cd .cache
+cd script
+python2 LITESCRIPT.py
+cd
+sh run
+fi
+
+
+if [ $pilih = 23 ] || [ $pilih = 23 ]
+then
+clear
+cd .cache
+cd SPAMer
+python2 SPAMer.py
+cd
+sh run
+fi
+
+if [ $pilih = 24 ] || [ $pilih = 24 ]
+then
+clear
+cd .cache
+cd brutal-sms
+python2 run.py
+sh run
+fi
+
+if [ $pilih = 25 ] || [ $pilih = 25 ]
+then
+clear
+cd .cache
+cd spamcall
+php call.php
+cd
+sh run
+fi
+
+if [ $pilih = 26 ] || [ $pilih = 26 ]
+then
+clear
+cd .cache
+cd Black-Fb-Premium
+python2 Black-Fb.py
+cd
+sh run
+fi
+
+
+if [ $pilih = 27 ] || [ $pilih = 27 ]
+then
+clear
+cd .cache
+cd darkfb
+python2 dark.py
+cd
+sh run
+fi
+
+
+if [ $pilih = 28 ] || [ $pilih = 28 ]
+then
+clear
+cd .cache
+cd darkfb2
+python2 Dark.py
+cd
+sh run
+fi
+
+
+if [ $pilih = 29 ] || [ $pilih = 29 ]
+then
+clear
+cd $HOME
+sh -c "$(curl -fsSL https://github.com/Cabbagec/termux-ohmyzsh/raw/master/install.sh)"
+cd
+zsh
+fi
+
+
+if [ $pilih = 30 ] || [ $pilih = 30 ]
+then
+clear
+cd .cache
+cd santet-online
+python2 santet.py
+cd
+sh run
+fi
+
+if [ $pilih = 0 ] || [ $pilih = 0 ]
+then
+clear
+cd .cache
+python2 telkomsel.py
+cd
+sh run
+fi
+
+
+if [ $pilih = 31 ] || [ $pilih = 31 ]
+then
+clear
+
+sh run
+fi
+
+
+if [ $pilih = 32 ] || [ $pilih = 32 ]
+then
+clear
+
+sh run
+fi
+
+
+if [ $pilih = 33 ] || [ $pilih = 33 ]
+then
+clear
+
+sh run
+fi
+
+
+if [ $pilih = 34 ] || [ $pilih = 34 ]
+then
+clear
+
+sh run
+fi
+
+
+if [ $pilih = 35 ] || [ $pilih = 35 ]
+then
+clear
+
+sh run
+fi
